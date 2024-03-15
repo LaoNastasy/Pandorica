54848a157385d4e1d075ecc373b6c610ef64ab2c
Кузин Александр <aleksandr.kuzin@gazprombank.ru>
2024-03-16 01:25:36 +0500

    MasterKey and EncKey generation

--- app/src/main/java/com/example/pandorica/network/entity/authorization/AuthBaseResponse.kt
+++ app/src/main/java/com/example/pandorica/network/entity/authorization/AuthBaseResponse.kt
@@ -3,5 +3,6 @@ package com.example.pandorica.network.entity.authorization
 open class AuthBaseResponse(
     open val accessToken: String,
     open val refreshToken: String,
-    open val encodedSecretKey: String
+    open val encodedSecretKey: String,
+    open val salt: String
 )
